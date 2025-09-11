85610fa46aa5728de9f9a5a6e484c7514c3108e2
engstk <eng.stk@sapo.pt>
2025-09-11 21:48:05 +0100

    Merge kernel_common (android14-6.1-2024-10_r25)
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- mm/workingset.c
+++ mm/workingset.c
@@ -16,6 +16,7 @@
 #include <linux/dax.h>
 #include <linux/fs.h>
 #include <linux/mm.h>
+#include <trace/hooks/mm.h>
 
 /*
  *		Double CLOCK lists
@@ -401,6 +402,8 @@ void workingset_refault(struct folio *folio, void *shadow)
 	int memcgid;
 	long nr;
 
+	trace_android_vh_count_workingset_refault(folio);
+
 	if (lru_gen_enabled()) {
 		lru_gen_refault(folio, shadow);
 		return;
@@ -408,6 +411,9 @@ void workingset_refault(struct folio *folio, void *shadow)
 
 	unpack_shadow(shadow, &memcgid, &pgdat, &eviction, &workingset);
 	eviction <<= bucket_order;
+
+	/* Flush stats (and potentially sleep) before holding RCU read lock */
+	mem_cgroup_flush_stats_ratelimited();
 
 	rcu_read_lock();
 	/*
@@ -463,8 +469,6 @@ void workingset_refault(struct folio *folio, void *shadow)
 	lruvec = mem_cgroup_lruvec(memcg, pgdat);
 
 	mod_lruvec_state(lruvec, WORKINGSET_REFAULT_BASE + file, nr);
-
-	mem_cgroup_flush_stats_delayed();
 	/*
 	 * Compare the distance to the existing workingset size. We
 	 * don't activate pages that couldn't stay resident even if
