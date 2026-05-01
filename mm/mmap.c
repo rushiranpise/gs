7a2b25df809211fdff97d6c5c6d60723cf72c430
engstk <eng.stk@sapo.pt>
2026-05-01 17:57:35 +0100

    Merge kernel_common (android14-6.1-2025-08_r11)
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3498,6 +3498,8 @@ struct vm_area_struct *copy_vma(struct vm_area_struct **vmap,
 		new_vma = vm_area_dup(vma);
 		if (!new_vma)
 			goto out;
+		/* Do not preserve padding flags on the new VMA */
+		vm_flags_clear(new_vma, VM_PAD_MASK);
 		new_vma->vm_start = addr;
 		new_vma->vm_end = addr + len;
 		new_vma->vm_pgoff = pgoff;
