b9160ad62b78bb588c0152327035091d3785fa93
engstk <eng.stk@sapo.pt>
2026-06-28 18:25:30 +0100

    drivers: kernelsu: update to v3.2.5
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/kernelsu/policy/app_profile.c
+++ drivers/kernelsu/policy/app_profile.c
@@ -108,7 +108,7 @@ int escape_with_root_profile(void)
     struct cred *cred;
     struct task_struct *p = current;
     struct task_struct *t;
-    struct root_profile profile;
+    struct root_profile *profile = NULL;
     struct user_struct *new_user;
 
     cred = prepare_creds();
@@ -122,20 +122,25 @@ int escape_with_root_profile(void)
         goto out_abort_creds;
     }
 
-    ksu_get_root_profile(cred->uid.val, &profile);
+    if (test_thread_flag(TIF_KSU_DISABLE_ESCAPE_WITH_ROOT)) {
+        pr_warn("TIF_KSU_DISABLE_ESCAPE_WITH_ROOT found, don't escape!\n");
+        goto out_abort_creds;
+    }
 
-    cred->uid.val = profile.uid;
-    cred->suid.val = profile.uid;
-    cred->euid.val = profile.uid;
-    cred->fsuid.val = profile.uid;
+    profile = ksu_get_root_profile(cred->uid.val);
 
-    cred->gid.val = profile.gid;
-    cred->fsgid.val = profile.gid;
-    cred->sgid.val = profile.gid;
-    cred->egid.val = profile.gid;
+    cred->uid.val = profile->uid;
+    cred->suid.val = profile->uid;
+    cred->euid.val = profile->uid;
+    cred->fsuid.val = profile->uid;
+
+    cred->gid.val = profile->gid;
+    cred->fsgid.val = profile->gid;
+    cred->sgid.val = profile->gid;
+    cred->egid.val = profile->gid;
     cred->securebits = 0;
 
-    BUILD_BUG_ON(sizeof(profile.capabilities.effective) != sizeof(kernel_cap_t));
+    BUILD_BUG_ON(sizeof(profile->capabilities.effective) != sizeof(kernel_cap_t));
 
     /*
      * Mirror the kernel set*uid path: update cred->user first, then
@@ -166,29 +171,32 @@ int escape_with_root_profile(void)
     }
 #endif
 
-    // setup capabilities
-    // we need CAP_DAC_READ_SEARCH becuase `/data/adb/ksud` is not accessible for non root process
-    // we add it here but don't add it to cap_inhertiable, it would be dropped automaticly after exec!
-    u64 cap_for_ksud = profile.capabilities.effective | CAP_DAC_READ_SEARCH;
-    memcpy(&cred->cap_effective, &cap_for_ksud, sizeof(cred->cap_effective));
-    memcpy(&cred->cap_permitted, &profile.capabilities.effective, sizeof(cred->cap_permitted));
-    memcpy(&cred->cap_bset, &profile.capabilities.effective, sizeof(cred->cap_bset));
+    memcpy(&cred->cap_effective, &profile->capabilities.effective, sizeof(cred->cap_effective));
+    memcpy(&cred->cap_permitted, &profile->capabilities.effective, sizeof(cred->cap_permitted));
+    memcpy(&cred->cap_bset, &profile->capabilities.effective, sizeof(cred->cap_bset));
 
-    setup_groups(&profile, cred);
-    setup_selinux(profile.selinux_domain, cred);
+    setup_groups(profile, cred);
+    setup_selinux(profile->selinux_domain, cred);
 
     commit_creds(cred);
 
     disable_seccomp();
 
+    if (profile->flags & FLAG_KSU_NO_NEW_PRIVS) {
+        set_thread_flag(TIF_KSU_DISABLE_ESCAPE_WITH_ROOT);
+    }
+
     for_each_thread (p, t) {
         ksu_set_task_tracepoint_flag(t);
     }
 
-    setup_mount_ns(profile.namespaces);
+    setup_mount_ns(profile->namespaces);
+    ksu_put_root_profile(profile);
     return 0;
 
 out_abort_creds:
+    if (profile)
+        ksu_put_root_profile(profile);
     abort_creds(cred);
     return ret;
 }
